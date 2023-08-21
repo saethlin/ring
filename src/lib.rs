00067260d35a830377877168d51f66e227032ff9
Ben Kimock <kimockb@gmail.com>
2023-08-21 20:01:36 +0000

    Checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
-mod cpu;
-mod digest;
-mod endian;
-
-pub static MYALGORITHM: &'static digest::MyAlgorithm = &digest::MYSHA256;
-
-/*
-extern "C" {
-    fn MyExampleFunction(state: &mut crate::digest::State, data: *const u8, num: crate::c::size_t);
-}
-
 pub struct MyAlgorithm {
-    inner: unsafe extern "C" fn(state: &mut crate::digest::State, data: *const u8, num: crate::c::size_t),
+    block_data_order: unsafe extern "C" fn(state: &mut State, data: *const u8, num: usize),
 }
 
-pub static MYSHA256: MyAlgorithm = MyAlgorithm {
-    inner: MyExampleFunction,
-};
+#[repr(C)]
+union State {
+    as64: [u64; 8],
+    as32: [u32; 8],
+}
 
-pub static DEMO: &'static inner::MyAlgorithm = &inner::MYSHA256;
-*/
+extern "C" {
+    fn GFp_sha256_block_data_order(state: &mut State, data: *const u8, num: usize);
+}
+
+mod digest {
+    pub static MYSHA256: super::MyAlgorithm = super::MyAlgorithm {
+        block_data_order: super::GFp_sha256_block_data_order,
+    };
+}
+
+pub static MYALGORITHM: &'static MyAlgorithm = &digest::MYSHA256;
 
 #[no_mangle]
 static mut GFp_armcap_P: u32 = 0;
