21fb283ab13beeec35353badbbb4192fa577a4be
Ben Kimock <kimockb@gmail.com>
2023-08-22 02:57:24 +0000

    Checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,12 @@
-pub struct FnWrapper {
-    inner: unsafe extern "C" fn(),
-}
-
 extern "C" {
     fn GFp_sha256_block_data_order();
 }
 
 mod digest {
-    pub static WRAPPER: super::FnWrapper = super::FnWrapper {
-        inner: super::GFp_sha256_block_data_order,
-    };
+    pub static WRAPPER: unsafe extern "C" fn() = super::GFp_sha256_block_data_order;
 }
 
-pub static WRAPPER_REF: &'static FnWrapper = &digest::WRAPPER;
+pub static WRAPPER_REF: &'static unsafe extern "C" fn() = &digest::WRAPPER;
 
 #[no_mangle]
 static mut GFp_armcap_P: u32 = 0;
