943bb5a508d1a77de3ad0e5c91c9621adfb5a0bb
Ben Kimock <kimockb@gmail.com>
2023-08-22 02:48:52 +0000

    Checkpoint

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 aarch64-linux-gnu-gcc sha256-armv8-linux64.S -I include/ -c -o sha256-armv8-linux64.o 
 aarch64-linux-gnu-ar rcs libring-core.a sha256-armv8-linux64.o 
 RUSTFLAGS="-Ccodegen-units=1024" cargo b --release --target=aarch64-unknown-linux-gnu
