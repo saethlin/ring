d7a47d2b30a49e62993b98741b6eb3d1f59a0588
Ben Kimock <kimockb@gmail.com>
2023-08-23 09:43:53 -0400

    Make portable

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-aarch64-linux-gnu-gcc sha256-armv8-linux64.S -I include/ -c -o sha256-armv8-linux64.o 
-aarch64-linux-gnu-ar rcs libring-core.a sha256-armv8-linux64.o 
-RUSTFLAGS="-Ccodegen-units=1024" cargo b --release --target=aarch64-unknown-linux-gnu
+cc ring-core.c -c -o ring-core.o
+ar rcs libring-core.a ring-core.o
+RUSTFLAGS="-Ccodegen-units=2" cargo b --release
